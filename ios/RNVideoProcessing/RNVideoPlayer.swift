4f66280274fdcdcac74999b9a1fa070ef622fc02
Rey Jonas Curimatmat <rjhonnas@gmail.com>
2019-09-30 15:19:44 +0800

    Revert "Xcode 10.2 (#216)"
    This reverts commit 01dd65ebb622b19ede7b3dd2e46cacf1c38ad3dd.

--- ios/RNVideoProcessing/RNVideoPlayer.swift
+++ ios/RNVideoProcessing/RNVideoPlayer.swift
@@ -33,7 +33,7 @@ class RNVideoPlayer: RCTView {
     var _replay: Bool = false
     var _rotate: Bool = false
     var isInitialized = false
-    var _resizeMode = AVLayerVideoGravity.resizeAspect
+    var _resizeMode = AVLayerVideoGravityResizeAspect
     var onChange: RCTBubblingEventBlock?
     
     let LOG_KEY: String = "VIDEO_PROCESSING"
@@ -64,10 +64,10 @@ class RNVideoPlayer: RCTView {
     
     var resizeMode: NSString? {
         set {
-            guard let newValue = newValue as String? else {
+            if newValue == nil {
                 return
             }
-            self._resizeMode = AVLayerVideoGravity(rawValue: newValue)
+            self._resizeMode = newValue as! String
             self.playerLayer?.videoGravity = self._resizeMode
             self.setNeedsLayout()
             print("CHANGED: resizeMode \(newValue)")
@@ -117,7 +117,7 @@ class RNVideoPlayer: RCTView {
                 let floatVal = convertedValue >= 0 ? convertedValue : self._playerStartTime
                 print("CHANGED: currentTime \(floatVal)")
                 if floatVal <= self._playerEndTime && floatVal >= self._playerStartTime {
-                    self.player.seek(to: convertToCMTime(val: floatVal), toleranceBefore: .zero, toleranceAfter: .zero)
+                    self.player.seek(to: convertToCMTime(val: floatVal), toleranceBefore: kCMTimeZero, toleranceAfter: kCMTimeZero)
                 }
             }
         }
@@ -236,7 +236,7 @@ class RNVideoPlayer: RCTView {
                     filterView.frame.size.height = self._playerWidth
                     filterView.bounds.size.width = self._playerHeight
                     filterView.bounds.size.height = self._playerWidth
-                    rotationAngle = CGFloat.pi / 2
+                    rotationAngle = CGFloat(M_PI_2)
                 } else {
                     filterView.frame.size.width = self._playerWidth
                     filterView.frame.size.height = self._playerHeight
@@ -291,17 +291,17 @@ class RNVideoPlayer: RCTView {
     }
     
     func toBase64(image: UIImage) -> String {
-        let imageData:NSData = image.pngData()! as NSData
+        let imageData:NSData = UIImagePNGRepresentation(image)! as NSData
         return imageData.base64EncodedString(options: .lineLength64Characters)
     }
     
     func convertToCMTime(val: CGFloat) -> CMTime {
-        return CMTimeMakeWithSeconds(Float64(val), preferredTimescale: Int32(NSEC_PER_SEC))
+        return CMTimeMakeWithSeconds(Float64(val), Int32(NSEC_PER_SEC))
     }
     
     func createPlayerObservers() -> Void {
         // TODO: clean obersable when View going to diesappear
-        let interval = CMTimeMakeWithSeconds(1.0, preferredTimescale: Int32(NSEC_PER_SEC))
+        let interval = CMTimeMakeWithSeconds(1.0, Int32(NSEC_PER_SEC))
         self.playerCurrentTimeObserver = self.player.addPeriodicTimeObserver(
             forInterval: interval,
             queue: nil,
@@ -340,7 +340,7 @@ class RNVideoPlayer: RCTView {
         
         if self.player == nil {
             player = AVPlayer()
-            player.volume = playerVolume.floatValue
+            player.volume = Float(self.playerVolume)
         }
         playerItem = AVPlayerItem(url: movieURL as! URL)
         player.replaceCurrentItem(with: playerItem)
@@ -381,8 +381,8 @@ class RNVideoPlayer: RCTView {
         super.willMove(toSuperview: newSuperview)
         if newSuperview == nil {
             
-            if let observer = self.playerCurrentTimeObserver {
-                self.player.removeTimeObserver(observer)
+            if self.playerCurrentTimeObserver != nil {
+                self.player.removeTimeObserver(self.playerCurrentTimeObserver)
             }
             if player != nil {
                 self.player.pause()
